30cd979baf8e7e46bf2ecfca69e53b8c6cbca915
Thilina Ranathunga <r.thilina@gmail.com>
2018-07-18 21:30:45 +0530

    Increase item quantity on receive

--- src/main/java/com/nanosl/inventory/service/ReceiveService.java
+++ src/main/java/com/nanosl/inventory/service/ReceiveService.java
@@ -1,17 +1,19 @@
 package com.nanosl.inventory.service;
 
+import java.util.Optional;
+
+import com.nanosl.inventory.domain.Item;
 import com.nanosl.inventory.domain.Receive;
+import com.nanosl.inventory.repository.ItemRepository;
 import com.nanosl.inventory.repository.ReceiveRepository;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
-import java.util.Optional;
 /**
  * Service Implementation for managing Receive.
  */
@@ -22,9 +24,11 @@ public class ReceiveService {
     private final Logger log = LoggerFactory.getLogger(ReceiveService.class);
 
     private final ReceiveRepository receiveRepository;
+    private final ItemRepository itemRepository;
 
-    public ReceiveService(ReceiveRepository receiveRepository) {
+    public ReceiveService(ReceiveRepository receiveRepository, ItemRepository itemRepository) {
         this.receiveRepository = receiveRepository;
+        this.itemRepository = itemRepository;
     }
 
     /**
@@ -34,7 +38,14 @@ public class ReceiveService {
      * @return the persisted entity
      */
     public Receive save(Receive receive) {
-        log.debug("Request to save Receive : {}", receive);        return receiveRepository.save(receive);
+        log.debug("Request to save Receive : {}", receive);
+
+        Item item = receive.getItem();
+        item = itemRepository.findById(item.getId()).get();
+        item.setQuantity(item.getQuantity() + receive.getQuantity());
+        itemRepository.save(item);
+
+        return receiveRepository.save(receive);
     }
 
     /**
@@ -48,7 +59,6 @@ public class ReceiveService {
         log.debug("Request to get all Receives");
         return receiveRepository.findAll(pageable);
     }
-
 
     /**
      * Get one receive by id.
